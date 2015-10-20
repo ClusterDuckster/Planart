20afab7b7ac386f013535bc5bec166112ed1ee90
Oliver Kaup <oliverkaup@gmx.de>
2015-10-20 03:44:33 +0200

    müsste eigentlich funktionieren...

--- Planet.js
+++ Planet.js
@@ -1,15 +1,21 @@
+var geometry, material, planetMesh;
+var bla;
+
 function Planet(){
-	var planetMesh;
-	
-	this.texture = "bla";
-	
+    
 	var loader = new THREE.TextureLoader();
 	loader.load( 'textures/land_ocean_ice_cloud_2048.jpg', function ( texture ) {
 
-	var geometry = new THREE.SphereGeometry( 2, 16, 16 );
-	var material = new THREE.MeshBasicMaterial( {map: texture, overdraw: 0.5} );
-	this.planetMesh = new THREE.Mesh( geometry, material );
+        geometry = new THREE.SphereGeometry( 2, 16, 16 );
+        //material = new THREE.MeshBasicMaterial( {map: texture, overdraw: 0.5} );
+        material = new THREE.MeshBasicMaterial( {color: 0x00ff00, wireframe: true} );
+        planetMesh = new THREE.Mesh( geometry, material );
 		
-	
+		bla = "bla";
+		
 	});
+    
+	this.texture = bla;
+	
+    this.planetMesh = new THREE.Mesh( geometry, material );
 }
