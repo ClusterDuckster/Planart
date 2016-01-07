37703648cba29c3dd97a3a7a401dec9ae8166af1
Oliver Kaup <oliverkaup@gmx.de>
2016-01-07 16:49:22 +0100

    Alles funzt

--- Planet.js
+++ Planet.js
@@ -1,21 +1,27 @@
-var geometry, material, planetMesh;
-var bla;
-
 function Planet(){
     
+    var myPlanet = this;
+
+	//Erstelle Planeten
+	this.geometry = new THREE.SphereGeometry( 2, 16, 16 );
+    //AUF KEINEN FALL THREE.MeshBasicMaterial() BENUTZEN!!! -> da kein emissive
+    this.material = new THREE.MeshLambertMaterial( {color: 0xffffff} );
+    this.mesh = new THREE.Mesh( this.geometry, this.material );
+
+	//Lade Textur
 	var loader = new THREE.TextureLoader();
 	loader.load( 'textures/land_ocean_ice_cloud_2048.jpg', function ( texture ) {
+		
+        myPlanet.mesh.material.map = texture;
+		myPlanet.mesh.material.needsUpdate = true;
+        //myPlanet.mesh.material.color = none;
 
-        geometry = new THREE.SphereGeometry( 2, 16, 16 );
-        //material = new THREE.MeshBasicMaterial( {map: texture, overdraw: 0.5} );
-        material = new THREE.MeshBasicMaterial( {color: 0x00ff00, wireframe: true} );
-        planetMesh = new THREE.Mesh( geometry, material );
-		
-		bla = "bla";
-		
+	},
+	function (xhr) {
+		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+	},
+	function ( xhr ) {
+		console.log( 'An error happened' );
 	});
-    
-	this.texture = bla;
 	
-    this.planetMesh = new THREE.Mesh( geometry, material );
 }
--- planart.js
+++ planart.js
@@ -1,56 +1,157 @@
-var scene, camera, renderer;
+var scene, camera, raycaster, renderer;
+var geometry, material, mesh;
+//Variables for Raycasting
+var mouse = new THREE.Vector2(), INTERSECTED;
 
 init();
 animate();
 
 function init() {
+
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera( 30, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-	renderer = new THREE.WebGLRenderer();
+    raycaster = new THREE.Raycaster();
+
+	renderer = new THREE.WebGLRenderer({antialias:true});
 	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.domElement.style.position = 'fixed';
+	renderer.domElement.style.zIndex = '-10';
 	document.body.appendChild( renderer.domElement );
 	
-	//TrackballControlls-> PAN:0 = Pan auf Linksklick
+	// TrackballControlls-> PAN:0 = Pan on left Mousebutton
 	controls = new THREE.TrackballControls( camera, renderer.domElement );
 	controls.noRotate = true;
 	controls.addEventListener( 'change', render );
 
-	// Planet 1
-	var geometry1 = new THREE.SphereGeometry( 2, 16, 16 );
-	var material1 = new THREE.MeshBasicMaterial( {color: 0xffff00, wireframe: true} );
-	var sphere1 = new THREE.Mesh( geometry1, material1 );
-	sphere1.position.x = -3;
+	var ambientLight = new THREE.AmbientLight(0xbbbbbb);
+    scene.add(ambientLight);
+
+
+    // Planet 1
+    geometry = new THREE.SphereGeometry( 2, 16, 16 );
+    material = new THREE.MeshLambertMaterial( {color: 0x00ff00} );
+    mesh = new THREE.Mesh ( geometry, material );
+
+    scene.add(mesh);
+
 
 	// Planet 2
-	var geometry2 = new THREE.SphereGeometry( 2, 16, 16 );
-	var material2 = new THREE.MeshBasicMaterial( {color: 0x00ff00, wireframe: true} );
-	var sphere2 = new THREE.Mesh( geometry2, material2 );
-	sphere2.position.x = 3;
-	
-	var planet1 = createPlanet();
-	planet1.position.x = -3
+    var geometry2 = new THREE.SphereGeometry( 2, 16, 16 );
+    var material2 = new THREE.MeshLambertMaterial( {} );
+    var sphere2 = new THREE.Mesh ( geometry2, material2 );
 
-	scene.add( planet1 );
-	scene.add( sphere2 );
+    sphere2.position.x = 5;
+
+    scene.add(sphere2);
+	
+	var loader = new THREE.TextureLoader();
+	loader.load( 'textures/land_ocean_ice_cloud_2048.jpg', function ( event ) {
+
+        var texture = event;
+        //texture.image.crossOrigin='';
+
+		sphere2.material.map = texture;
+        //sphere2.material.overdraw = 0.5;
+        sphere2.material.needsUpdate = true;
+
+        console.log(sphere2);
+
+
+
+	},
+	function (xhr) {
+		console.log( (xhr.loaded / xhr.total * 100) + "% loaded" );
+	},
+	function ( xhr ) {
+		console.log( "An error happened" );
+	});
+
+
+	//Create planet object
+	var planet1 = createPlanet();
+	planet1.mesh.position.x = -5;
 
 	camera.position.z = 30;
 
-	render();
+    //Add Listeners
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+    window.addEventListener( 'resize', onWindowResize, false );
+}
+
+//
+function onDocumentMouseMove( event ) {
+
+    event.preventDefault();
+
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    //console.log("x:"+mouse.x+"  y:"+mouse.y);
+
 }
 
 function animate() {
 	requestAnimationFrame( animate );
+    render();
 	controls.update();
 }
 
 function render() {
+
+    camera.updateMatrixWorld();
+
+
+
+    // find intersections
+	//Source: http://threejs.org/examples/#webgl_interactive_cubes
+    raycaster.setFromCamera( mouse, camera );
+
+    var intersects = raycaster.intersectObjects( scene.children );
+
+    if ( intersects.length > 0 ) {
+
+        if ( INTERSECTED != intersects[ 0 ].object ) {
+
+            if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+
+            INTERSECTED = intersects[ 0 ].object;
+            INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+            INTERSECTED.material.emissive.setHex( 0x003366 );
+
+        }
+
+    }
+
+    else {
+
+        if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+
+            INTERSECTED = null;
+
+    }
+
 	renderer.render(scene, camera);
 }
 
 function createPlanet(){
+
 	var newPlanet = new Planet();
-	console.log(newPlanet.texture);
 	
-	return newPlanet.planetMesh;
+    scene.add(newPlanet.mesh);
+
+	return newPlanet;
+}
+
+function onWindowResize() {
+
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
 }
